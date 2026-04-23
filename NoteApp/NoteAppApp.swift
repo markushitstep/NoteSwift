ab7b9aa542be5a36f059bec640c9a17a50f700db
Max <markushitstep@gmail.com>
2026-04-23 16:50:55 +0300

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Маркуш Максим on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
