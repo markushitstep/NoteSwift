7b7fd9554fac2e9267c74d4da4519c8a182859b3
Max <markushitstep@gmail.com>
2026-04-27 14:30:43 +0300

    add init to Note struct

--- NoteApp/Models/Note.swift
+++ NoteApp/Models/Note.swift
@@ -1,8 +1,23 @@
 import Foundation
 
-struct Note: Identifiable {
-    let id = UUID()
+struct Note: Identifiable, Codable {
+    let id: UUID
     let text: String
     let date: Date
     let temperature: Double
+    let weatherDescription: String
+
+    init(
+        id: UUID = UUID(),
+        text: String,
+        date: Date,
+        temperature: Double,
+        weatherDescription: String
+    ) {
+        self.id = id
+        self.text = text
+        self.date = date
+        self.temperature = temperature
+        self.weatherDescription = weatherDescription
+    }
 }
